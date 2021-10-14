f7cd5c2592081bdf492c5d7800a961c2d29fd1cd
gcsbruno <brunoguimaraes.cs@gmail.com>
2021-10-13 23:35:32 -0300

    contact dinamico

--- myapp/lib/screens/contacts_list.dart
+++ myapp/lib/screens/contacts_list.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:myapp/models/contact.dart';
 import 'package:myapp/screens/contacts_form.dart';
 
 class ContactsList extends StatelessWidget {
+  final List<Contact> contacts = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,20 +13,12 @@ class ContactsList extends StatelessWidget {
         automaticallyImplyLeading: true,
         title: Text('Contatos'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-              child: ListTile(
-            title: Text('Bruno',
-                style: TextStyle(
-                  fontSize: 24.0,
-                )),
-            subtitle: Text(
-              '1000',
-              style: TextStyle(fontSize: 16.0),
-            ),
-          ))
-        ],
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final Contact contact = contacts[index];
+          return _ContactItem(contact);
+        },
+        itemCount: contacts.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -38,6 +32,31 @@ class ContactsList extends StatelessWidget {
         },
         child: Icon(
           Icons.add,
+        ),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  _ContactItem(this.contact);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: TextStyle(
+            fontSize: 24.0,
+          ),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: TextStyle(
+            fontSize: 16.0,
+          ),
         ),
       ),
     );
